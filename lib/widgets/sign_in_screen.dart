403d2237b4e71f8a0e8d5b0ba21ece062f52a0de
QusDev <qusdeveloper@gmail.com>
2024-10-23 17:28:33 +0300

    clear project and create empty screens

---
+++ lib/widgets/sign_in_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
